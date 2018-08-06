15b62b062e0604e351a19b998544c3b53a5ab722
vlargius <vlargius@gmail.com>
2018-08-06 16:55:42 +0300

    [-] obsolete files

--- rectangle.h
+++ rectangle.h
@@ -13,6 +13,8 @@ struct Color {
 	}
 };
 
+using uchar = unsigned char;
+
 struct MyRectangle
 {
 	vec2d pos;
@@ -25,7 +27,7 @@ struct MyRectangle
 		width(width),
 		height(height),
 		pos(pos){
-		Color c{ rand () % 255, rand() % 255, rand () % 255, 0 };
+		Color c{ (uchar) (rand () % 255), (uchar) (rand() % 255), (uchar) (rand () % 255), (uchar)( 0) };
 		color = c.to_uni();
 	}
 
